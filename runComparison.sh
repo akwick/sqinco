76d7f05cb217ff7bdd0a3723bb0ee16a1b970f01
akwick <anna-katharina.wickert@mailbox.org>
2016-12-16 10:31:05 +0100

    Adopt comparison to public repository

--- runComparison.sh
+++ runComparison.sh
@@ -5,5 +5,5 @@ safesql goretech/comparison
 echo "Analyse with gas"
 gas -include=G201,G202 ./...
 echo "Analyse with our tool"
-analysis -src="main.go" -ssf="sqlQuery.txt" -pkgs="goretech/comparison/sqlInjections"
+gotcha -src="main.go" -ssf="sqlQuery.txt" -pkgs="github.com/akwick/sqinco/sqlInjections"
 
